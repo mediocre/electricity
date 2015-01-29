4c314d7b76172fc1196a030b4ad19b22662de85f
Harrison Massey <harrison@mediocre.com>
2015-01-29 17:06:32 -0600

    Include jsx test files

---
+++ test/public/jsx/reactInclude.js
@@ -0,0 +1,7 @@
+var Include = React.createClass({
+    render: function() {
+        return (
+            <h1>This got included!</h1>
+        );
+    }
+});
---
+++ test/public/jsx/reactSnockets.js
@@ -0,0 +1,3 @@
+//= require reactInclude.js
+
+React.render(<Include />, document.body);
