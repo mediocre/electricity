15636ca359bbcb412e2b78ca13e7e81c1e9d23e7
Shawn Miller <shawn@mediocre.com>
2016-06-17 22:18:11 -0500

    Initial commit

--- test/public/jsx/compiled/reactSnockets.js
+++ test/public/jsx/compiled/reactSnockets.js
@@ -1,7 +1,11 @@
-var Include = React.createClass({displayName: "Include",
-    render: function() {
-        return (
-            React.createElement("h1", null, "This got included!")
+var Include = React.createClass({
+    displayName: "Include",
+
+    render: function () {
+        return React.createElement(
+            "h1",
+            null,
+            "This got included!"
         );
     }
 });
--- test/public/jsx/compiled/reactTest.js
+++ test/public/jsx/compiled/reactTest.js
@@ -1,5 +1,6 @@
 //Test comment
-React.render(
-    React.createElement("h1", null, "Shocked by electricity!"),
-    document.body
-);
+React.render(React.createElement(
+    "h1",
+    null,
+    "Shocked by electricity!"
+), document.body);
